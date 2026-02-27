310eb7db2bcffda5092936a02d9e9626dc0978b5
Ryann Joshua Lleno <gadessa@Ryanns-MacBook-Air.local>
2026-02-27 20:59:31 +0800

    Fix case sensitivity for Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
 import Amenities from './components/Amenities';
